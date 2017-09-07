61e832b333fd66a3627867ee07a26548a721cffc
Matthew Silversmith <msilversmith@ufl.edu>
2017-09-06 22:19:32 -0400

    Update queries.js

--- queries.js
+++ queries.js
@@ -5,6 +5,9 @@ var findLibraryWest = function() {
     Find the document that contains data corresponding to Library West,
     then log it to the console. 
    */
+  Listing.find({name: "Library West"}, function(err, result) {
+  console.log("Found " + result);
+  })
 };
 var removeCable = function() {
   /*
@@ -12,17 +15,30 @@ var removeCable = function() {
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
     and remove this listing from your database and log the document to the console. 
    */
+  Listing.findOneAndRemove({ code: 'CABL' }, function(err, listing) {
+  if (err) throw err;
+  console.log("Deleted " + listing);
+  });
 };
 var updatePhelpsMemorial = function() {
   /*
     Phelps Memorial Hospital Center's address is incorrect. Find the listing, update it, and then 
     log the updated document to the console. 
    */
+  Listing.findOneAndUpdate({ name: 'Phelps Laboratory' }, { address: '701 N Broadway' }, function(err, listing) {
+  if (err) throw err;
+  console.log("Updated " + listing);
+});
 };
 var retrieveAllListings = function() {
   /* 
     Retrieve all listings in the database, and log them to the console. 
    */
+  Listing.find({}, function(err, listing) {
+  if (err) throw err;
+  console.log(listing);
+  mongoose.connection.close();
+});
 };
 
 findLibraryWest();
